77fba13dd6264f675215fe5fa53d85c4948e831c
Alfat1989 <89654590+Alfat1989@users.noreply.github.com>
2022-09-18 16:19:51 +0000

    Deploying to gh-pages from @ Alfat1989/goit-react-hw-05-movies@bd3c0f63b40ae9548d369747ee541915a8e9554b 🚀

---
+++ static/js/112.709f50d1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[112],{2112:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var r=n(184),i=function(e){var t=e.title;return(0,r.jsx)("h1",{children:t})}}}]);
+//# sourceMappingURL=112.709f50d1.chunk.js.map
---
+++ static/js/545.081cfe0c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[545],{9545:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h1",{children:"PAGES IS NOT FOUND"})})}}}]);
+//# sourceMappingURL=545.081cfe0c.chunk.js.map
